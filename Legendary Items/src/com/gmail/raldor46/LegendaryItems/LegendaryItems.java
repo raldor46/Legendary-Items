643b50c501ae515e24d6154ab6a5f19b9cc30ec5
raldor46 <raldor46@gmail.com>
2013-07-05 09:10:35 -0400

    Signed-off-by: raldor46 <raldor46@gmail.com>

--- Legendary Items/src/com/gmail/raldor46/LegendaryItems/LegendaryItems.java
+++ Legendary Items/src/com/gmail/raldor46/LegendaryItems/LegendaryItems.java
@@ -1,20 +1,69 @@
 package com.gmail.raldor46.LegendaryItems;
 
+import java.sql.SQLException;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LegendaryItems extends JavaPlugin {
+	LegendarySwords swords;
 	public void onEnable() {
-		
+		swords = new LegendarySwords();
 	}
 	public void onDisable() {
 		
 	}
 	@Override
-	public boolean onCommand(CommandSender sender, Command command,
-			String label, String[] args) {
-		// TODO Auto-generated method stub
-		return super.onCommand(sender, command, label, args);
+	public boolean onCommand(CommandSender sender, Command command,String label, String[] args) {
+		if (sender instanceof Player){
+			if (command.getName().equalsIgnoreCase("Legendary")){
+				if(args.length > 1 && args.length < 4){
+					if(args[0].equalsIgnoreCase("Sword")){
+						if (args[1].equalsIgnoreCase("Get")){
+							try {
+								if(swords.validName(args[2])){
+									ItemStack sword = swords.genSword(args[2]);
+									if(sword != null){
+										((Player) sender).getInventory().addItem(sword);
+										return true;
+									}
+									else{
+										sender.sendMessage("unable to create sword");
+										return false;
+									}
+								}
+								else{
+									sender.sendMessage("Invalid Name");
+									return false;
+								}
+							} catch (IllegalArgumentException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (SQLException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+						else{
+							sender.sendMessage(args[1]+" not implemented.");
+							return false;
+						}
+					}
+					else{
+						sender.sendMessage(args[0]+ " is not a valid item");
+						return false;
+					}
+				}
+				else{
+					sender.sendMessage("Not enough Arguments");
+					return false;
+				}
+			}
+		}
+		
+		return false;
 	}
 }
--- Legendary Items/src/com/gmail/raldor46/LegendaryItems/LegendarySwords.java
+++ Legendary Items/src/com/gmail/raldor46/LegendaryItems/LegendarySwords.java
@@ -1,17 +1,47 @@
 package com.gmail.raldor46.LegendaryItems;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Logger;
+
+import org.bukkit.inventory.ItemStack;
+
+import com.gmail.raldor46.Items.Sword;
 
 import lib.PatPeter.SQLibrary.MySQL;
 
-public class LegendarySwords {
+public class LegendarySwords{
 	static MySQL sql = new MySQL(Logger.getLogger("Minecraft"), 
-            "[Minecraft] ", 
+            "minecraft", 
             "localhost", 
             3306, 
             "minecraft", 
             "minecraft", 
             "minecraft");
 	
-
+	public ItemStack genSword(String name) throws SQLException{
+		ResultSet results = sql.query("SELECT * FROM `minecraft`.`legendary_swords`WHERE Name = '"+ name + "'");
+		if(results.first()){
+		Sword s = new Sword(results.getString("Name"),results.getString("Material"),results.getString("Lore"));
+			
+			s.setSharpness_Level(results.getInt("Sharpness_Level"));
+			s.setSmite_Level(results.getInt("Smite_Level"));
+			s.setBane_Level(results.getInt("Bane_Level"));
+			s.setKnockback_Level(results.getInt("Knockback_Level"));
+			s.setFireAspect_Level(results.getInt("FireAspect_Level"));
+			s.setLooting_Level(results.getInt("Looting_Level"));
+			s.setUnbreaking_Level(results.getInt("Unbreaking_Level"));
+		
+		return s.createSword();
+		}
+		return null;
+	}
+	
+	public boolean validName(String Name) throws SQLException{
+		ResultSet results = sql.query("SELECT Name FROM `minecraft`.`legendary_swords`WHERE Name = '"+ Name + "'");
+		if(results.first()){
+			return true;
+		}
+		return false;
+	}
 }
